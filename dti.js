1c206c7a699276e6410a33aa6921f31c5da7ec2d
Alexis <88553880+jonesam01@users.noreply.github.com>
2022-05-30 10:52:43 -0500

    Finished calculator

--- dti.js
+++ dti.js
@@ -3,20 +3,15 @@
 let grossIncome; 
 let debtTotal;
 let result;
-// let result = [];
 let percentDTI;
 
 
 function computeDTI() {
-    // event.preventDefault();
     grossIncome = document.getElementById('gross-income').value;
     debtTotal = document.getElementById('debts').value;
-    // result = document.querySelector('#results');
 
     percentDTI = Math.floor((debtTotal / grossIncome) * (100));
     
-    // result.push("percentDTI");
     document.getElementById('results').innerHTML = percentDTI;
-    // return false;
 }
 
