3677e11dc683fc36d3b731ac073b3317d76ece1a
Alexis <88553880+jonesam01@users.noreply.github.com>
2022-05-30 10:33:12 -0500

    Coding results

--- dti.js
+++ dti.js
@@ -3,18 +3,21 @@
 let grossIncome; 
 let debtTotal;
 let result;
+// let result = [];
+let percentDTI;
 
 
-function computeDTI(event, inputText) {
+function computeDTI() {
     // event.preventDefault();
     grossIncome = document.getElementById('gross-income').value;
     debtTotal = document.getElementById('debts').value;
     // result = document.querySelector('#results');
 
     percentDTI = ((debtTotal / grossIncome) * (100));
-    alert('your dti is ' + percentDTI + '%');
-    document.getElementById('results').innerHTML = percentDTI; 
+    // alert('your dti is ' + percentDTI + '%');
     
+    // result.push("percentDTI");
+    document.getElementById('results').innerHTML = percentDTI;
     // return false;
 }
 
